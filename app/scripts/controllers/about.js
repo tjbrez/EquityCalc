64568f6cb970d0a59c0e66af3b042c465c65b547
“Thomas <bres2709@yahoo.com>
2014-11-05 20:09:24 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name equityCalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the equityCalcApp
+ */
+angular.module('equityCalcApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
